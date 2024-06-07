57362014d865b2f0edec78c2ef525e29fb07dc02
rpkak <rpkak@users.noreply.github.com>
2024-06-07 16:25:07 +0200

    init

---
+++ build.zig
@@ -0,0 +1,115 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    {
+        const exe = b.addExecutable(.{
+            .name = "zig-blake3",
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        b.installArtifact(exe);
+
+        const options = b.addOptions();
+        options.addOption(bool, "c", false);
+        options.addOption(bool, "std", false);
+
+        exe.root_module.addOptions("config", options);
+    }
+
+    {
+        const exe = b.addExecutable(.{
+            .name = "zig-std-blake3",
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        b.installArtifact(exe);
+
+        const options = b.addOptions();
+        options.addOption(bool, "c", false);
+        options.addOption(bool, "std", true);
+
+        exe.root_module.addOptions("config", options);
+    }
+
+    const c_path = b.path("BLAKE3/c");
+
+    {
+        const exe = b.addExecutable(.{
+            .name = "c-asm-blake3",
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+
+        exe.addSystemIncludePath(c_path);
+        exe.addCSourceFiles(.{
+            .root = c_path,
+            .files = &.{
+                "blake3.c",
+                "blake3_dispatch.c",
+                "blake3_portable.c",
+                "blake3_sse2_x86-64_unix.S",
+                "blake3_sse41_x86-64_unix.S",
+                "blake3_avx2_x86-64_unix.S",
+            },
+            .flags = &.{"-DBLAKE3_NO_AVX512"},
+        });
+        exe.linkLibC();
+
+        b.installArtifact(exe);
+
+        const options = b.addOptions();
+        options.addOption(bool, "c", true);
+        options.addOption(bool, "std", false);
+
+        exe.root_module.addOptions("config", options);
+    }
+
+    {
+        const exe = b.addExecutable(.{
+            .name = "c-blake3",
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+
+        exe.addSystemIncludePath(c_path);
+        exe.addCSourceFiles(.{
+            .root = c_path,
+            .files = &.{
+                "blake3.c",
+                "blake3_dispatch.c",
+                "blake3_portable.c",
+                "blake3_sse2.c",
+                "blake3_sse41.c",
+                "blake3_avx2.c",
+            },
+            .flags = &.{"-DBLAKE3_NO_AVX512"},
+        });
+        exe.linkLibC();
+
+        b.installArtifact(exe);
+
+        const options = b.addOptions();
+        options.addOption(bool, "c", true);
+        options.addOption(bool, "std", false);
+
+        exe.root_module.addOptions("config", options);
+    }
+
+    const tests = b.addTest(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_tests = b.addRunArtifact(tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_tests.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+const config = @import("config");
+const blake3 = @import("./root.zig");
+const c = @cImport({
+    @cInclude("blake3.h");
+});
+
+const stderr = std.io.getStdErr();
+const stdout = std.io.getStdOut();
+
+fn run() !void {
+    if (std.os.argv.len != 2) {
+        return error.OneArgRequired;
+    }
+
+    const fd = try std.posix.openZ(std.os.argv[1], .{}, undefined);
+    defer std.posix.close(fd);
+
+    const stat = try std.posix.fstat(fd);
+
+    const area = try std.posix.mmap(null, @intCast(stat.size), 1, .{ .TYPE = .PRIVATE }, fd, 0);
+    defer std.posix.munmap(area);
+
+    var out: [32]u8 = undefined;
+
+    if (config.c) {
+        var hasher: c.blake3_hasher = undefined;
+        c.blake3_hasher_init(&hasher);
+        c.blake3_hasher_update(&hasher, area.ptr, area.len);
+        c.blake3_hasher_finalize(&hasher, &out, out.len);
+    } else if (config.std) {
+        std.crypto.hash.Blake3.hash(area, &out, .{});
+    } else {
+        blake3.Blake3(.{}).hash(area, &out);
+    }
+
+    try stdout.writer().print("{s}\n", .{std.fmt.bytesToHex(out, .lower)});
+}
+
+pub fn main() u8 {
+    run() catch |err| {
+        stderr.writer().print("Error: {}\n", .{err}) catch unreachable;
+        return 1;
+    };
+    return 0;
+}
